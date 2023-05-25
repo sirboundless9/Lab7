d48919ff70a38a82aa8ab9e7b4068dab445a9227
sirboundless9 <115053571+sirboundless9@users.noreply.github.com>
2023-05-25 22:04:50 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -40,7 +40,7 @@ def check(name):
 
 def get(name):
     id = check(name)
-    cursor.execute("""select rate from currency_rates_values 
+    cursor.execute("""select rate from currency_rates 
                               where  currency_rate_id = %s""", (id,))
     data_id = cursor.fetchall()
 
@@ -64,7 +64,7 @@ async def payload(Request: RequestBody):
         id_cur = check(name)
         print(id_cur)
         for i in rates:
-            cursor.execute(""" Insert into currency_rates_values (currency_code,rate,currency_rate_id)
+            cursor.execute(""" Insert into currency_rates (currency_code,rate,currency_rate_id)
                                          values (%s,%s,%s);""", (i.code, i.rate, id_cur,))
             conn.commit()
         raise HTTPException(200)
